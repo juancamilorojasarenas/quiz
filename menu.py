5ee3348784cc64f5d176b9a015f6ace7857ae13c
Juan camilo rojas arenas <119133501+juancamilorojasarenas@users.noreply.github.com>
2025-03-31 08:41:35 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from utilidades import*
+from utilidades import primos_gemelos, palindromico
 menu_principal="""
 *****************************************
 bienvenido usuari@ presione
@@ -8,5 +8,20 @@ bienvenido usuari@ presione
 *****************************************
 
 """
-opc=input("ingresa tu opcion wacho: ")
 
+def menu():
+
+    while True:
+        print(menu_principal)
+        opc=input("ingresa tu opcion: ")
+        if opc=="1":
+            limite = int(input("Por favor ingrese el límite superior: "))
+            primos_gemelos(2,limite)
+        elif opc=="2":
+            limite = int(input("Por favor ingrese el límite superior: "))
+            palindromico(2, limite)
+        elif opc=="3":
+            print("saliendo....")
+            break
+        else:
+            print("opcion no valida lea bien ")

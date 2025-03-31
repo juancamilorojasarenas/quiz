0509371b677259b6244a2af553ee6889e74f9020
Juan camilo rojas arenas <119133501+juancamilorojasarenas@users.noreply.github.com>
2025-03-31 08:41:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from menu import menu_principal, menu_dos, menu_uno, opc
-while True:
-    print(menu_principal)
-    if opc=="1":
-        print("")
-    elif opc=="2":
-        print("")
-    elif opc=="3":
-        print("saliendo....")
-        break
-    else:
-        print("opcion no valida lea bien ")
-        
+from menu import menu
+
+menu()        
